86006130443535bdbceba2ed2e3bf528dc204a14
ldb-258 <102220094@sv1.dut.udn.vn>
2025-10-16 14:25:30 +0700

    fix: assets module missing provider inject

--- apps/content-service/src/assets/assets.module.ts
+++ apps/content-service/src/assets/assets.module.ts
@@ -4,10 +4,16 @@ import { AssetsMaintenanceController } from './assets-maintenance.controller';
 import { AssetsService } from './assets.service';
 import { AssetsMaintenanceService } from './assets-maintenance.service';
 import { CloudinaryProvider } from './cloudinary.provider';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [AssetsController, AssetsMaintenanceController],
-  providers: [CloudinaryProvider, AssetsService, AssetsMaintenanceService],
+  providers: [
+    CloudinaryProvider,
+    AssetsService,
+    AssetsMaintenanceService,
+    PrismaService,
+  ],
   exports: [CloudinaryProvider, AssetsService, AssetsMaintenanceService],
 })
 export class AssetsModule {}
